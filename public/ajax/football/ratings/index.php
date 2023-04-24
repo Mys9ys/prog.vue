f0b423e29d46cf7035eeed0f2c110e366c972086
a.avdiuk <a.avdiuk@eurokappa.ru>
2023-04-24 18:15:30 +0300

    приступил к рейтингам 2.9 95%

--- public/ajax/football/ratings/index.php
+++ public/ajax/football/ratings/index.php
@@ -184,7 +184,6 @@ class CreateFootballRatings{
                     if($score !== $prev) {
                         $place = $count;
                     }
-                    $el['num'] = $count;
 
                     $el['place'] = $place;
                     $el['user'] =  $this->arUsers[$uid];
@@ -192,14 +191,7 @@ class CreateFootballRatings{
 
                     if($id>1 && $selector != 'best') {
                         $el['diff'] = $this->arResults[$selector][$id-1][$uid]['place'] - $place;
-
-//                        var_dump($el['diff']);
-//                        var_dump($selector);
-//                        var_dump($place);
-//                        var_dump($el['user']['name']);
-//                        var_dump($this->arResults[$selector][$id-1][$uid]);
-//                        var_dump($this->arResults[$selector][$id-1][$uid]['place']);
-//                        die;
+                        if($place === abs($el['diff'])) $el['diff'] = 0;
                     } else {
                         $el['diff'] = 0;
                     }
@@ -210,22 +202,13 @@ class CreateFootballRatings{
                 }
                 $this->arResults[$selector][$id] = $arSorted;
             }
-//            foreach ($match as $id=>$scores){
-//                foreach ($scores as $num=>$score){
-//                    if($id == 1){
-//                        $score['diff'] = 0;
-//                    } else {
-//                        $score['diff'] = $score['place'] - $match[$id][$scores[$num]]['place'];
-//                    }
-//                    $this->arResults[$selector][$id] = $score;
-//                }
-//            }
+
         }
         foreach ($this->arResults as $selector=>$match){
 
             foreach ($match as $id=>$scores) {
 
-                array_multisort(array_column($scores, 'тгь'), SORT_DESC, $scores);
+                array_multisort(array_column($scores, 'score'), SORT_DESC, $scores);
 
                 $this->arResults[$selector][$id] = $scores;
             }
--- src/components/football/SelectBlockRating.vue
+++ src/components/football/SelectBlockRating.vue
@@ -5,7 +5,7 @@
     </select>
     <div v-for="(data, index) in arRating" :key="index">
       <div v-if="selected==index">
-        <table class="table table-dark table-hover om_table_box">
+        <table class="table table-dark table-hover rating_table_box">
           <thead>
           <tr>
             <th class="pr_table_col">#</th>
@@ -17,7 +17,9 @@
           <tbody>
           <tr v-for="(el) in data" :key="el.id">
             <td class="pr_table_col">{{el.place}}</td>
-            <td class="pr_table_col">{{el.diff}} </td>
+            <td class="pr_table_col" v-if="el.diff === 0"><div class="zero_diff">–</div></td>
+            <td class="pr_table_col" v-else-if="el.diff >0"><div class="plus_diff">{{el.diff}}</div></td>
+            <td class="pr_table_col" v-else-if="el.diff <0"><div class="minus_diff">{{el.diff}}</div></td>
             <td class="pr_table_col">{{el.user.name}}</td>
             <td class="pr_table_col">{{el.score}}</td>
           </tr>
@@ -66,5 +68,23 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+@import "src/assets/css/variables.less";
+.rating_table_box{
+  th,td{
+    padding: 2px;
+    text-align: left;
+  }
+}
+.zero_diff{
+  min-width: 20px;
+  color: @pearl;
+  font-size: 16px;
+  font-weight: 700;
+}
+.plus_diff{
+  color: @YesWrite;
+}
+.minus_diff{
+  color: @red;
+}
 </style>
