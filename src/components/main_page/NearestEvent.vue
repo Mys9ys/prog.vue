4387d58939f4463dd1f7263e4ec5a759004dda8a
Mys9ys9ka <rjbexa@yandex.ru>
2023-10-05 12:17:51 +0300

    плашка с названием ивента на страницу "ближайшие"

--- src/components/main_page/NearestEvent.vue
+++ src/components/main_page/NearestEvent.vue
@@ -20,21 +20,44 @@
           <div class="empty" >событий нет</div>
         </div>
         <div v-else>
-          <div class="events_for" v-for="(items, event) in arrNearest[period]['items']" :key="event">
+          <div class="events_for" v-for="(items, event) in arrNearest[period]" :key="event">
             <div class="race" v-if="event === 'race'">
               <EventRace
-                  v-for="(el, index) in items"
+                  v-for="(el, index) in items['items']"
                   :key="index"
                   :element="el"
               ></EventRace>
             </div>
 
             <div class="football" v-if="event === 'football'">
-              <EventMatch
-                  v-for="(el, index) in items"
-                  :key="index"
-                  :match="el"
-              ></EventMatch>
+              <div class="football_event_box" v-for="(events, event_id) in items" :key="event_id">
+                <div class="event_title">
+                  <div class="img_box">
+                    <img :src="urlImg+events['info']['img']" alt="">
+                  </div>
+                  <div class="info_box">
+                    <div class="name">{{events['info']['PREVIEW_TEXT']}}</div>
+                    <div class="btn_box">
+                      <div class="count">Матчей: {{Object.keys(events['items']).length}}</div>
+                      <div class="btn_block">
+                        <div class="event_btn" @click="$router.push('/championship/'+event +'/' +event_id)">
+                          <span class="text">Таблица</span>
+                          <img class="arrow" src="@/assets/icon/pagination/right.svg" alt="">
+                        </div>
+                        <div class="event_btn">
+                          <span class="text">Список</span>
+                          <img class="arrow" src="@/assets/icon/pagination/right.svg" alt="">
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <EventMatch
+                    v-for="(el, index) in events['items']"
+                    :key="index"
+                    :match="el"
+                ></EventMatch>
+              </div>
             </div>
           </div>
         </div>
@@ -68,8 +91,9 @@ export default {
         'yesterday': {'name': 'Вчера', 'count': 0, 'set': 0},
         'today': {'name': 'Сегодня', 'count': 0, 'set': 0},
         'tomorrow': {'name': 'Завтра', 'count': 0, 'set': 0},
-      }
-
+      },
+      // link: '/football/' + this.match.event + '/' + this.match.number,
+      urlImg: 'https://prognos9ys.ru/'
     }
   },
 
@@ -138,7 +162,7 @@ export default {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  margin-bottom: 10px;
+  margin-bottom: 4px;
 
   .period {
     display: flex;
@@ -215,10 +239,91 @@ export default {
   gap: 5px;
   margin-bottom: 5px;
 }
-.football{
+
+.football_event_box{
   display: flex;
   flex-direction: column;
   gap: 2px;
+
+  margin-top: 8px;
+
+  .event_title{
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    padding: 4px;
+    color: @color;
+    border-radius: 5px;
+    text-align: left;
+    gap: 4px;
+
+    background: @YesWrite2;
+
+    .img_box{
+      width: 52px;
+      height: 52px;
+      .shadow_inset;
+      //.flex_center;
+      background: @colorText;
+      img{
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    .info_box{
+      width:100%;
+      display: flex;
+      flex-direction: column;
+      gap:4px;
+
+      .name{
+        width:100%;
+        font-size: 16px;
+        .shadow_inset;
+        .flex_center;
+        justify-content: left;
+      }
+
+      .btn_box{
+        display: flex;
+        flex-direction: row;
+        gap:4px;
+        justify-content: space-between;
+
+        .count{
+          .flex_center;
+          .shadow_inset;
+          font-size: 12px;
+        }
+
+        .btn_block{
+          display: flex;
+          flex-direction: row;
+          gap:4px;
+
+          .event_btn{
+            min-width: 70px;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            gap: 3px;
+            font-size: 12px;
+            color: @colorText;
+            .shadow_template;
+            padding: 2px;
+            border-radius: 5px;
+            .text{
+              width: 80%;
+            }
+            .arrow{
+              width: 10px;
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 </style>
